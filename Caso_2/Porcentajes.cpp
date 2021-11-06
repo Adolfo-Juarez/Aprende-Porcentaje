3b647a1948a7bee8d8ef40346f9106e1820a6c1e
Adolfo-Juarez <213358@ids.upchiapas.edu.mx>
2021-11-05 21:24:49 -0600

    Función de porcentaje

--- Caso_2/Porcentajes.cpp
+++ Caso_2/Porcentajes.cpp
@@ -9,10 +9,26 @@
 using namespace std;
 
 int Explicacion (float a, int b){ //Función para obtener el porcentaje con los datos dados en entradadeDatos (y explicar al niño)
-	float r;
+	float r; //Variable para guardar el resultado de la operación
+
+	cout << "Usted ha ingresado el valor "<<a<<", esto representa el 100%"<< endl;
+	cout << "para calcular el "<< b << "% de esta cantidad podemos emplear muchos métodos"<<endl;
+	cout << "\n"<< endl;
+	r=Porcentaje (a,b);
+
+	
 }
 
-int entradadeDatos (){ //Función para entrada de datos
+float Porcentaje (float a, int b){
+	float c,d;
+
+	c=a*100;
+	d=c/b;
+
+	return d;
+}
+
+void entradadeDatos (){ //Función para entrada de datos
 	float porcentaje; //Variable para saber qué porcentaje obtener
 	int base; //Variable del número a obtener porcentaje
 	bool Check; //Resvisor para bucles de error
