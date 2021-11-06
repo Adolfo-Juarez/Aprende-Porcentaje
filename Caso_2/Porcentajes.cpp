6238669c8c96ca5d75fc9d81504af96994052d30
Adolfo-Juarez <213358@ids.upchiapas.edu.mx>
2021-11-05 20:52:35 -0600

    Otro uno

--- Caso_2/Porcentajes.cpp
+++ Caso_2/Porcentajes.cpp
@@ -3,20 +3,38 @@
 
 using namespace std;
 
-int entradadeDatos (){
-    float porcentaje; //Variable para saber qué porcentaje obtener
-    int base; //Variable del número a obtener porcentaje
+int Explicacion (float a, int b){ //Función para obtener el porcentaje con los datos dados en entradadeDatos (y explicar al niño)
+	float r;
+}
 
-    bool Check; //Resvisor para bucles de error
-    cout << "Bienvenido a la calculadora de porcentajes\n"<<endl; //Saludo inicial
-    cout << "Ingrese la cantidad inicial para obtener su porcentaje: ";
-    cin >> base;
-    cout <<"\n"<<endl; //Salto de línea
-    cout <<"Por favor ingrese el porcentaje que desea calcular (1-100): ";
-    cin >> porcentaje; //Entrada del dato del porcentaje
+int entradadeDatos (){ //Función para entrada de datos
+	float porcentaje; //Variable para saber qué porcentaje obtener
+	int base; //Variable del número a obtener porcentaje
+	bool Check; //Resvisor para bucles de error
 
+	cout << "Bienvenido a la calculadora de porcentajes\n"<<endl; //Saludo inicial
+	do{
+		cout << "Ingrese la cantidad inicial para obtener su porcentaje: ";
+		cin >> base;
+		if (base <= 0){
+			cout << "Ingrese una cifra un poco más grande \nInténte un número mayor a 0";
+			cout <<"\n"<<endl; //Salto de línea
+		}
+		else{
+			cout <<"\n"<<endl; //Salto de línea
+			cout <<"Por favor ingrese el porcentaje que desea calcular (1-100): ";
+			cin >> porcentaje; //Entrada del dato del porcentaje
+			if (porcentaje <= 100 && porcentaje >= 1){
+				Explicacion (porcentaje, base);
+			}
+			else{
+				cout << "Ingrese una cifra un poco más grande \nInténte un número mayor a 0";
+				cout <<"\n"<<endl; //Salto de línea
+			}
+		}
+	}while (Check==false);
 }
 
 int main (){
-
+	entradadeDatos();
 }
