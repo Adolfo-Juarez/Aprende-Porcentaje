269d5ec6b6b705c7a0f524ef1b3daf0533b3d122
Adolfo-Juarez <213358@ids.upchiapas.edu.mx>
2021-11-05 22:56:33 -0600

    AaAaAA

--- Caso_2/Porcentajes.cpp
+++ Caso_2/Porcentajes.cpp
@@ -8,22 +8,28 @@
 
 using namespace std;
 
-bool Explicacion (float p, float b){ //Función para obtener el porcentaje con los datos dados en entradadeDatos (y explicar al niño) [PROCESO 1]
+void Explicacion (float p, float b){ //Función para obtener el porcentaje con los datos dados en entradadeDatos (y explicar al niño) [PROCESO 1]
 	
 	float r;
 
-	cout << "Usted ha ingresado el valor "<<b<<", esto representa el 100%"<< endl;
-	cout << "para calcular el "<< p << "% de esta cantidad podemos emplear muchos métodos, los cuales veremos más adelante"<<endl;
+	cout << "\n"<<endl;
+	cout <<"_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _"<<endl;
+	cout << "¿Cómo representar un porcentaje?"<<endl;
+	cout <<"_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n"<<endl;
+	cout << "Usted ha ingresado el valor "<<b<<", esto representa el 100%\n"<< endl;
+	cout << "Para calcular el "<< p << "% de esta cantidad podemos emplear muchos métodos, los cuales veremos más adelante"<<endl;
 	cout << "\n"<< endl;
-	cout <<"De igual forma, podemos representar dsichos porcentajes en fraccione o decimales\nPor ejemplo:"<<endl;
-	cout << "Para representar un porcentaje en fracción, sólo basta poner en el numerador el porcentaje deseado y en el denominador\nel número 100, pues el porcentaje es una cantidad fraccionaria dividida entre cien (de ahí el nombre, porCENTAJE), para representar tú decisión\nsolo basta escribirlo de la siguiente forma: "<<p<<"/100\n"<<endl;
+	cout <<"De igual forma, podemos representar dichos porcentajes en fraccione o decimales\n\nPor ejemplo:"<<endl;
+	cout << "Para representar un porcentaje en fracción, sólo basta poner en el numerador el porcentaje deseado y en el denominador el número 100, \npues el porcentaje es una cantidad fraccionaria dividida entre cien (de ahí el nombre, porCENTAJE), para representar tú decisión\nsolo basta escribirlo de la siguiente forma: "<<p<<"/100\n"<<endl;
 	r = p/100.00;
-	cout << "Para representar un porcentaje en decimal, solo basta realizar la fracción anterior, es decir, dividir el porcentaje entre 100. \nEn este caso sería \n\n"<<p<<"/100 = "<<r<<"\n\nde esta forma, obtenemos el porcentaje en su forma decimal."<<endl;
+	cout << "Para representar un porcentaje en decimal, solo basta realizar la fracción anterior, es decir, dividir el porcentaje entre 100. \nEn este caso sería \n"<<p<<"/100 = "<<r<<"\nde esta forma, obtenemos el porcentaje en su forma decimal."<<endl;
+	cout << "\n\nPresione cualquier tecla para continuar";
+	cin >> r;
 
 
 }
 
-float Porcentaje (float a, int b){ //Obtener porcentaje de los valores dados, para poder transformar los valores de la funcion explicacions sin problemas
+float Result (float a, int b){ //Obtener porcentaje de los valores dados, para poder transformar los valores de la funcion explicacions sin problemas
 	float c,d;
 
 	c=a*100;
@@ -32,16 +38,45 @@ float Porcentaje (float a, int b){ //Obtener porcentaje de los valores dados, pa
 	return d;
 }
 
-int ExplainA (float a, int b){ //Función para explicar el procedimiento del modo A
-	float r;
+void Porcentaje (float p, float b){ //Explicación de obtención de los porcentajes
+	int eleccion;
+	bool Check;
+	cout << "\n"<<endl;
+	cout <<"_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _"<<endl;
+	cout << "¿Cómo calcular el porcentaje de una cantidad?"<<endl;
+	cout <<"_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n"<<endl;
+	cout << "El porcentaje de una cantidad se puede calcular de varias formas, como lo mencionábamos antes:"<<endl;
+	do{
+		cout << "1) Multiplicar por el porcentaje y dividir entre 100 \n 2) Mulplicar en forma decimal.\n\n¿Cuál deseas aprender?"<<endl;
+		cin >>eleccion;
+	switch (eleccion)
+	{
+	case 1:
+		ExplainA (p,b);
+		Check = true;
+		break;
+	case 2:
+		ExplainB (p,b);
+		Check = true;
+	default:
+	cout << "Ups, parece que ha ingresado una opción no válida, inténtalo de nuevo"<<endl;
+	cout << "\n"<<endl;
+		break;
+	}
+	}while (Check==false);
 
-	r = Porcentaje (a,b);
 }
 
-int ExplainB (float a, int b){ //Función para explicar el procedimiento del modo B
+int ExplainA (float a, float b){ //Función para explicar el procedimiento del modo A
 	float r;
 
-	r = Porcentaje (a,b);
+	r = Result (a,b);
+}
+
+int ExplainB (float a, float b){ //Función para explicar el procedimiento del modo B
+	float r;
+
+	r = Result (a,b);
 }
 
 void entradadeDatos (){ //Función para entrada de datos
