36dffdbbc0c731bcbb925c4d0b46e1db582e0d52
Adolfo-Juarez <213358@ids.upchiapas.edu.mx>
2021-11-05 20:15:59 -0600

    Entrada y salida de datos

--- Caso_2/Porcentajes.cpp
+++ Caso_2/Porcentajes.cpp
@@ -1,12 +1,22 @@
-#include <iostream>
-#include <cmath>
+#include <iostream> //Entrada y salida de datos
+#include <cmath> //Operaciones aritméticas avanzadas 
 
 using namespace std;
 
 int entradadeDatos (){
+    float porcentaje; //Variable para saber qué porcentaje obtener
+    int base; //Variable del número a obtener porcentaje
+
+    bool Check; //Resvisor para bucles de error
+    cout << "Bienvenido a la calculadora de porcentajes\n"<<endl; //Saludo inicial
+    cout << "Ingrese la cantidad inicial para obtener su porcentaje: ";
+    cin >> base;
+    cout <<"\n"<<endl; //Salto de línea
+    cout <<"Por favor ingrese el porcentaje que desea calcular (1-100): ";
+    cin >> porcentaje; //Entrada del dato del porcentaje
 
 }
 
-int main (){ //bhjg
+int main (){
 
 }
